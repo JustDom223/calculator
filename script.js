daaef6ad974c1784fe7e964bc91eda107f562c72
JustDom223 <124432802+JustDom223@users.noreply.github.com>
2023-10-13 19:41:16 +0200

    the buttons now change the number in the screen The buttons need to be disconnected so that only the numbers change the screen

--- script.js
+++ script.js
@@ -1,8 +1,13 @@
 const btnContainerElement = document.getElementById('btnContainer')
+const currentNumberElement = document.querySelector('.currentNumber')
+const lastNumberElement = document.querySelector('.lastNumber')
+
+
 
 btnContainerElement.addEventListener('click', function (event){
     if(event.target.tagName === 'BUTTON'){
-        console.log('btnClicked' + event.target.textContent)
+        console.log(event.target.value)
+        currentNumberElement.textContent = event.target.value
     }
 })
 
